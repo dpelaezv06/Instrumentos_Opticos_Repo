11ad05b549f974f41ac6d046e1a2ac24972cfb23
Jose Diaz <jodiazgu@unal.edu.co>
2024-11-28 13:33:28 -0500

    Correción Espectro Angular
    Faltaban los shifts adecuados de las FFT

--- espectro_Angular.py
+++ espectro_Angular.py
@@ -24,21 +24,21 @@ import puntos_Mascaras as pts
 
 longitud_onda = 632.8e-6                                #longitud de onda de un Láser de He-Ne
 numero_onda = 2*np.pi/longitud_onda
-ventana = 20                                            #Ventana de 5cm
-resolucion = 3000                                       #1024 puntos
+ventana = 10                                            #Ventana de 5cm
+resolucion = 2048                                       #1024 puntos
 radio = 1                                               #Radio de 1.5mm para el círculo 
-Distancia_z = 10                                       #Distancia al plano de observación
+Distancia_z = 158.03                                    #Distancia al plano de observación en mm
 
 '''Funciones para calcular la difracción '''
 deltas = pts.producto_EspacioFrecuencia(ventana, resolucion)                                #Regresa los delta espacio, frecuencia en un diccionario
 X_in, Y_in = pts.malla_Puntos(resolucion, ventana)                                          #Se prepara una malla de puntos para la máscara
 X_espectre, Y_espectre = pts.malla_Puntos(resolucion, resolucion*deltas["Delta_F"])
 mascara = pts.funcion_Circulo(radio, None, X_in,Y_in)                                       #Se crea la mascara de un círculo, este va a ser el Campo U[x,y,0] de entrada
-espectro_0 = (deltas["Delta_X"]**2) * np.fft.fft2(mascara)                                 #Se calcusla A[x,y,0]
+espectro_0 = (deltas["Delta_X"]**2) * np.fft.fftshift(np.fft.fft2(mascara))                 #Se calcula   la A[x,y,0]
 termino_propagante = np.exp(1j*Distancia_z*numero_onda*np.sqrt(1-((longitud_onda**2) * ((X_espectre**2) + (Y_espectre**2)))))
 espectro_propagante = espectro_0 * termino_propagante        #Calculamos el espectro A[x,y,z]
-Campo_Propagante = (deltas["Delta_F"]**2) * np.fft.ifft2(espectro_propagante) #Calculamos el campo U[x,y,z] y lo shifteamos
-intensidad_Salida = np.abs(Campo_Propagante)**2
+Campo_Propagante = (deltas["Delta_F"]**2) * np.fft.fftshift(np.fft.ifft2(espectro_propagante)) #Calculamos el campo U[x,y,z] y lo shifteamos
+intensidad_Salida = np.fft.fftshift(np.abs(Campo_Propagante)**2)
 
 ''' GRAFICAS '''
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))  # Crear dos subgráficos (uno para el plano de abertura y otro para el plano de salida)
