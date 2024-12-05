b00acec3b9190c0d4c2254e45fe99ee3b4cd4a28
dpelaezv06 <dpelaezv@unal.edu.co>
2024-12-05 15:34:15 -0500

    Ahora dice el tiempo de ejecución
    Se agregaron algunas líneas para mirar el tiempo de ejecución del código

--- Transformada_Fresnel.py
+++ Transformada_Fresnel.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import diffraction_library as diff
+import time
 
 ''' FUNCIONES PROPIAS DEL PROGRAMA'''
 def producto_EspacioFrecuenciaFresnel (longitud_Onda, distancia_Propagacion, intervalo, resolucion): #Calculo del producto espacio frecuencia en transformada de fresnel
@@ -14,6 +15,8 @@ def producto_EspacioFrecuenciaFresnel (longitud_Onda, distancia_Propagacion, int
     deltas = {"delta_Salida": delta_Salida, "delta_Llegada": delta_Llegada} #ponemos ambos deltas en un diccionario
     return deltas #retornamos el diccionario con los deltas
 
+#Función para calcular el tiempo que tarda el código
+reloj_1 = time.time()
 
 ''' ENTRADAS '''
 #Relativas a la fuente de iluminacion
@@ -46,6 +49,12 @@ campo_SalidaSinEscalar = np.fft.fft2(campo_EntradaParabolico) #calculamos la fft
 campo_Salida = np.fft.fftshift(campo_SalidaSinEscalar) * (deltas_Espacio["delta_Salida"] ** 2) * fase_Constante * fase_ParabolicaSalida #escalamos el campo de salida con las constantes
 
 intensidad_Salida = (np.abs(campo_Salida)) ** 2 #calculamos el patron de difraccion sacando modulo cuadrado
+
+
+#Funciones para calcular el tiempo de ejecucion
+reloj_2 = time.time()
+tiempo = reloj_2 - reloj_1
+print("Tiempo de ejecucion: ", tiempo)
 
 ''' GRAFICAS '''
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))  # Crear dos subgráficos (uno para el plano de abertura y otro para el plano de salida)
