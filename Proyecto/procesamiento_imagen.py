9e40aa22fc9a063631607261a345ae3c0c3fa7cb
dpelaezv06 <dpelaezv@unal.edu.co>
2025-03-05 23:12:43 -0500

    Mejoramiento de comentarios

--- Proyecto/procesamiento_imagen.py
+++ Proyecto/procesamiento_imagen.py
@@ -3,32 +3,30 @@ from scipy.ndimage import shift
 
 def desplazar_imagen(imagen, x_min, x_max, y_min, y_max, distancia, angulo):
     """
-    Desplaza un mapa de alturas en una dirección y magnitud especificadas.
+    Desplaza un mapa de intensidad en una dirección y una distancia
 
     Parámetros:
-    - mapa: np.ndarray, matriz 2D representando el mapa de alturas.
-    - x_min, x_max: float, límites físicos en la dirección X (metros).
-    - y_min, y_max: float, límites físicos en la dirección Y (metros).
-    - distancia: float, distancia del desplazamiento en metros.
-    - angulo: float, ángulo en grados (0° es hacia la derecha, 90° hacia arriba).
+    - imagen: matriz 2D representando el mapa de alturas
+    - x_min, x_max: float, límites físicos en la dirección X (metros)
+    - y_min, y_max: float, límites físicos en la dirección Y (metros)
+    - distancia: float, distancia del desplazamiento en metros
+    - angulo: float, ángulo en grados
 
     Retorna:
-    - mapa_desplazado: np.ndarray, el mapa de alturas desplazado.
+    - imagen_desplazada: np.ndarray, el mapa de alturas desplazado.
     """
-    # Tamaño de la imagen en píxeles
-    filas, columnas = mapa.shape
 
-    # Tamaño del pixel en metros
-    pixel_x = (x_max - x_min) / columnas
+    filas, columnas = imagen.shape #extraemos el tamano de la imagen en pixeles 
+
+    pixel_x = (x_max - x_min) / columnas #asignamos un tamano fisico a la imagen
     pixel_y = (y_max - y_min) / filas
 
-    # Convertir distancia en píxeles
-    desplazamiento_x = distancia * np.cos(np.radians(angulo)) / pixel_x
-    desplazamiento_y = -distancia * np.sin(np.radians(angulo)) / pixel_y  # Negativo porque en NumPy el eje Y crece hacia abajo
+    desplazamiento_x = distancia * np.cos(np.radians(angulo)) / pixel_x #se convierte la distancia de desplazamiento en pixeles
+    desplazamiento_y = -distancia * np.sin(np.radians(angulo)) / pixel_y  #es necesario que sea negativo, porque al tratarse de una matriz, el numero del pixel crece hacia abajo
 
-    # Aplicar desplazamiento con interpolación de orden 0 (mantiene valores y rellena con ceros)
-    mapa_desplazado = shift(mapa, shift=(desplazamiento_y, desplazamiento_x), mode='constant', cval=0, order=0)
 
-    return mapa_desplazado
+    imagen_desplazada = shift(imagen, shift=(desplazamiento_y, desplazamiento_x), mode='constant', cval=0, order=0) #desplazamos la imagen y rellenamos los vacios con cero
+
+    return imagen_desplazada #retornamos la imagen desplazada 
 
 
